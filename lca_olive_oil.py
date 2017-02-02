f43c13c981c19ddeee3cd0e032eefe0a7b98ba3a
oneyb <zenlines@gmail.com>
2017-02-02 09:46:42 +0100

    getting organized

---
+++ lca_olive_oil.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+#  A script with an LCA calculation of olive oil using a SimaPro data set and Brightway2
+#
+#  Copyright (C) 2016  Brian J. Oney, brian.j.oney|at|gmail.com
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+
+desc = """
+Write me :)
+"""
+
+if '-h' in sys.argv or '--help' in sys.argv or len(sys.argv) == 1 and __name__ == '__main__':
+    print desc
+    sys.exit(0)
+
+import re
+
+def :
+    """
+
+    """
+    return result
+
+
+
+# Run if wanted
+if __name__ == '__main__':
+    for filename in sys.argv[1:]:
+        rename_pdf(filename)
