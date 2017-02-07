841cf8b35cf6584a2e2230c9c93d557c16404c47
oneyb <zenlines@gmail.com>
2017-02-07 19:16:15 +0100

    stopping for now

--- install-brightway-functions.bash
+++ install-brightway-functions.bash
@@ -1,45 +1,28 @@
 # from: https://docs.brightwaylca.org/advanced-installation.html#linux
 # for python 3 on debian jessy
 
-function install-deps-of-brightway ()
+# In my .bashrc
+function get-bash-functions-for-bw2 ()
 {
-    sudo apt-get update
-    sudo apt-get install python3-scipy python3-numpy python3-nose python3-pip     \
-         python3-lxml cython python3-virtualenv virtualenvwrapper build-essential \
-         libsuitesparse-dev swig mercurial
-    # # I had to remove ipython: old debian stuff
-    #  sudo apt-get remove ipython3*
+    source /d/documents/eaternity/eaternity/install-brightway-functions.bash
 }
 
-function install-conda()
-{
-    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-    bash Miniconda3-latest-Linux-x86_64.sh
-    dir=/d/documents/eaternity/bw2-py
-    echo $dir
-    conda install -q -y conda && conda update -q conda
-}
-
-
-function install-brightway-dev-plus-deps-on-conda()
+function flip-bw2-virtualenv-conda ()
 {
     dir=/d/documents/eaternity/bw2-py
     cd $dir
-    conda install -q -y conda && conda update -q conda
-    conda config --system --add channels conda-forge
-    conda create -y -n bw2 python=3.5
-    source activate bw2
-    cd ..
-    conda install wheel && conda update -q pip wheel setuptools
-    conda install -q -y -c haasad pypardiso
-    conda install -q -y ipython ipython-notebook jupyter matplotlib flask lxml \
-          requests nose docopt whoosh xlsxwriter xlrd unidecode appdirs future \
-          psutil unicodecsv wrapt numpy
-    pip install --no-cache-dir brightway2
-    conda clean -tipsy
-    conda install networkx seaborn matplotlib
-    pip install https://bitbucket.org/cmutel/activity-browser/get/2.0.zip
-
+    if [ -z "which conda" ]; then
+        echo activating
+        export PATH="$dir/bin:$PATH"
+        source activate bw2
+        PROMPT_COMMAND='echo -ne "\033]0;Brightway2\007"'
+    else
+        echo deactivating
+        source deactivate
+        PROMPT_COMMAND='echo -ne "\033]0;Terminal\007"'
+        export PATH=$(echo $PATH | sed -r 's/^[^:]+://g')
+    fi
+    cd -
 }
 
 
@@ -61,7 +44,7 @@ function install-brightway-dev-plus-deps-on-debian ()
 
     pip3 install -U wheel setuptools notebook jupyter numpy
     pip3 install eight bw2speedups scikit-umfpack
-    pip3 install --user brightway2
+
 
     if [[ $2 = "dev" ]]; then
         pip3 install -e hg+https://bitbucket.org/cmutel/brightway2-data#egg=bw2data
@@ -97,7 +80,6 @@ function uninstall-brightway-dev-plus-deps ()
          scikit-umfpack brightway2
 
     pip3 uninstall bw2data bw2calc bw2ui bw2analyzer
-
     # Finish
     xdotool type deactivate
 }
@@ -115,19 +97,66 @@ function turn-on-bw2-virtualenv ()
         deactivate
         PROMPT_COMMAND='echo -ne "\033]0;Terminal\007"'
     fi
-    echo Restart emacs daemon?
+    echo "Restart emacs daemon?"
     xdotool type red
-    # # Or
-	  # if [[ -n "`pgrep -f emacs`" ]]; then
-	  #     killall -w 'emacs'
-	  # fi
-	  # emacs --daemon
 }
 
-function red()
+function install-deps-of-brightway ()
 {
-	  if [[ -n "`pgrep -f emacs`" ]]; then
-	      killall -w 'emacs'
-	  fi
-	  emacs --daemon
+    sudo apt-get update
+    sudo apt-get install python3-scipy python3-numpy python3-nose python3-pip     \
+         python3-lxml cython python3-virtualenv virtualenvwrapper build-essential \
+         libsuitesparse-dev swig mercurial
+}
+
+function install-conda()
+{
+    dir=/d/documents/eaternity/bw2-py
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+    bash Miniconda3-latest-Linux-x86_64.sh
+    echo $dir
+    conda install -q -y conda && conda update -q conda
+}
+
+function install-brightway-dev-plus-deps-with-conda ()
+{
+    if [[ $# -eq 0 ]]; then
+        dir=/d/documents/eaternity/bw2-py
+    else
+        dir=$1
+    fi
+    if [ -f $dir ]; then
+        cd $dir
+    else
+        mkdir -p $dir
+    fi
+
+    conda config --system --add channels conda-forge
+    conda create -y -n bw2 python=3.5
+
+    source activate bw2
+    cd -
+
+    conda install wheel && conda update -q pip wheel setuptools
+    conda install -q -y -c haasad pypardiso
+    conda install -q -y ipython ipython-notebook jupyter matplotlib flask lxml \
+          requests nose docopt whoosh xlsxwriter xlrd unidecode appdirs future \
+          psutil unicodecsv wrapt numpy
+    pip install --no-cache-dir brightway2
+    conda clean -tipsy
+    # Activity browser
+    conda install networkx seaborn matplotlib
+    pip install https://bitbucket.org/cmutel/activity-browser/get/2.0.zip
+    # if [[ $2 = "dev" ]]; then
+    #     pip3 install -e hg+https://bitbucket.org/cmutel/brightway2-data#egg=bw2data
+    #     pip3 install -e hg+https://bitbucket.org/cmutel/brightway2-calc#egg=bw2calc
+    #     pip3 install -e hg+https://bitbucket.org/cmutel/brightway2-ui#egg=bw2ui
+    #     pip3 install -e hg+https://bitbucket.org/cmutel/brightway2-analyzer#egg=bw2analyzer
+    # else
+    #     pip3 install bw2data bw2calc bw2ui bw2analyzer
+    # fi
+
+    # Finish
+    deactivate
+    cd -
 }
