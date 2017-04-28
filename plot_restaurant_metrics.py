58ee5e9cc9e57ecc08aeaa0b54f961a7dc0eaae6
oneyb <zenlines@gmail.com>
2017-04-28 20:37:33 +0200

    started playing around with data

--- plot_restaurant_metrics.py
+++ plot_restaurant_metrics.py
@@ -1,13 +1,61 @@
-#!/usr/bin/env python
+
+# coding: utf-8
+
+# In[19]:
 
 import config
 import pandas as pd
+import seaborn as sns
 import os
-# import gzip
+get_ipython().magic('matplotlib inline')
+pd.set_option('display.notebook_repr_html', False)
+pd.set_option('display.max_columns', 10)
+pd.set_option('display.max_rows', 10)
+
+
+# In[5]:
 
 if 'data' not in globals().keys():
     fpath = os.path.join(config.DHOME, 'merged.csv.gz')
-    print(fpath)
+    # print(fpath)
     data = pd.read_csv(fpath, compression='gzip', encoding='mac_roman')
 
-print(data.head())
+
+
+# In[6]:
+
+# set date column
+data['dt'] = pd.to_datetime(data.Date, format='%m-%Y')
+data.set_index(['dt'], inplace=True)
+
+
+# In[7]:
+
+data.info()
+
+
+# In[8]:
+
+# No. of restaurants
+len(data['name'].unique())
+
+
+# In[10]:
+
+get_ipython().magic('pinfo data.groupby')
+
+
+# In[16]:
+
+get_ipython().magic('pinfo pd.Series.sort_values')
+
+
+# In[18]:
+
+data.groupby(['Kitchen_ID'])['Total_CO2_kg'].median().sort_values(ascending=False)
+
+
+# In[ ]:
+
+sns.
+
